6c55c42e84973bf78b24c6507f9b4e88fad5bab0
raninaiswarya <raninaiswarya@gmail.com>
2021-08-11 14:34:45 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,6 +33,28 @@ export class AppComponent implements OnInit {
     if (this.registerForm.invalid) {
       return;
     }
+
+    let receiveddata = localStorage.getItem('datas');
+    let data = [];
+    data = JSON.parse(receiveddata);
+    console.log('test', receiveddata);
+    if (data.length == 0) {
+      console.log('datas', data);
+      localStorage.setItem('datas', JSON.stringify(data));
+      // this.registerForm.patchValue(this.clearing);
+      this.submitted = false;
+      this.registerForm.reset();
+      alert('Saved Successfully');
+    } else {
+      let senddata = [];
+      senddata.push(this.registerForm.value);
+      localStorage.setItem('datas', JSON.stringify(senddata));
+      this.submitted = false;
+      this.registerForm.reset();
+      // this.intermediateForm.patchValue(this.clearing);
+      alert('Saved Successfully');
+    }
+
     let senddata = [];
     // display form values on success
     // + JSON.stringify(this.registerForm.value, null, 4));
